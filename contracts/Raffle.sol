4d83a12ca8fc07071fa3fd224928c69c6e837807
Do Xuan Long <longdo@dobtech.vn>
2023-02-18 19:24:22 +0700

    track player

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -7,17 +7,31 @@ pragma solidity ^0.8.17;
 // Winner to be selected every X minutes -> completly automate
 // Chainlink Oracle -> Randomness, Automated Execution (Chainlink Kepper)
 
+error Raffle__NotEnoughETHEntered();
+
 contract Raffle {
+  // State Variables
   uint256 private immutable i_entranceFee;
+  address payable[] private s_players;
 
   constructor(uint256 entranceFee) {
     i_entranceFee = entranceFee;
   }
 
-  // function enterRaffle() {}
+  function enterRaffle() public payable {
+    if (msg.value < i_entranceFee) {
+      revert Raffle__NotEnoughETHEntered();
+    }
+    s_players.push(payable(msg.sender));
+  }
+
   // function pickRandomWinner() {}
 
   function getEntranceFee() public view returns (uint256) {
     return i_entranceFee;
+  }
+
+  function getPlayer(uint256 index) public view returns (address) {
+    return s_players[index];
   }
 }
