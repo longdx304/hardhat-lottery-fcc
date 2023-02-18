6b6061b28818e8356300ad217b12a7a5b61b56ea
Do Xuan Long <longdo@dobtech.vn>
2023-02-18 19:05:21 +0700

    scaffold project

---
+++ contracts/Raffle.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+// Raffle
+// Enter the lottery (paying some amount)
+// Pick a random winner (verifiably random)
+// Winner to be selected every X minutes -> completly automate
+// Chainlink Oracle -> Randomness, Automated Execution (Chainlink Kepper)
+
+contract Raffle {
+    // function enterRaffle() {}
+    // function pickRandomWinner() {}
+}
